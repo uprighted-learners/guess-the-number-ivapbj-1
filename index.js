8372001619f9952ab6228befbca7bfd9f739be1a
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-03-04 21:08:41 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+function prompt(promptText) {
+  return new Promise((resolve, reject) => {
+    rl.question(promptText, resolve);
+  });
+}
+
+async function start() {
+  await guessTheNumberGame();
+}
+async function guessTheNumberGame() {
+  console.log(
+    "Let's play a game where you (human) make up a number and I `(computer) try to guess it."
+  );
+  let max = await prompt("Choose your range greater than 1 \n");
+  console.log(`Your range will be between 1 - ${max}`);
+  let secretNumber = await prompt(
+    "What is your secret number?\nI won't peek, I promise...\n"
+  );
+  console.log(`You entered: ${secretNumber}`);
+  if (secretNumber < 0 || secretNumber > 100 || isNaN(secretNumber)) {
+    console.log(
+      "Please, make sure to input a number between 1 - 100. Start over!"
+    );
+    process.exit();
+  } else {
+    console.log("Let's begin!");
+  }
+  function getRandomNumberFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  let min = 1;
+  const maxAttempts = Math.floor(Math.log2(max) + 1);
+
+  let randomNumber = getRandomNumberFromRange(min, max);
+  let attempts = 1;
+  //games asks you to guess number
+  while (true) {
+    if (attempts > maxAttempts) {
+      console.log("But I've run out of attempts... play again");
+      process.exit();
+    } else {
+      let guessResponse = await prompt(
+        `Attempt Number ${attempts}/${maxAttempts}.\nI think your number is ${randomNumber}.\nIs this right? Is your number higher or lower?\n   (R) - Right\n   (H) - Higher\n   (L) - Lower\n`
+      );
+      let guess = guessResponse.toLowerCase(); //sanitze input to lowercase
+      if (guess === "r") {
+        console.log(`Yay! I guessed correctly!\n End of game.`);
+        process.exit(); // write call back function outside of start to restart game
+      } else if (guess === "h") {
+        console.log(`Ok. The number is higher.`);
+        min = randomNumber + 1;
+        randomNumber = getRandomNumberFromRange(min, max);
+      } else if (guess === "l") {
+        console.log(`Ok. The number is lower.`);
+        max = randomNumber - 1;
+        randomNumber = getRandomNumberFromRange(min, max);
+      } else {
+        console.log(
+          `Please answer (R) - Right\n   (H) - Higher\n   (L) - Lower\n `
+        );
+      }
+      attempts++; // increment attempts by 1
+    }
+  }
+}
+
+start();
+// ~ to run this code, open Terminal and enter 'node index.js'
---
+++ reverse-game.js
@@ -0,0 +1,44 @@
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+async function prompt(promptText) {
+  return new Promise((resolve, reject) => {
+    rl.question(promptText, resolve);
+  });
+}
+
+async function start() {
+  await guessTheNumber();
+}
+async function guessTheNumber() {
+  // program picks a random number between 1-100
+  const secretNumber = Math.floor(Math.random() * 101);
+  // console.log("secretNumber", secretNumber);
+  // tells info needed to play game
+  console.log(
+    "Let's play a game where I (computer) make up a number and you (human) try to guess it correctly"
+  );
+
+  //games asks you to guess number
+  let userGuess = await prompt(`Whats the number you want to guess 1-100?\n`);
+  userGuess = parseInt(userGuess);
+
+  while (true) {
+    // check to see if the user guessed the secret number correctly
+    if (parseInt(userGuess) === secretNumber) {
+      console.log(" Yay, you did it!");
+      process.exit();//replace this with a call to function- to run the start function(its a callback)
+
+      // if not, determine if the number that the user guessed is higher or lower
+    } else if (userGuess > secretNumber) {
+      console.log(`try a lower number`);
+    } else {
+      console.log(`try a higher number`);
+    }
+    userGuess = parseInt(await prompt("Guess again \n"));
+  }
+
+}
+
+start();
+// ~ To run this code, open Terminal and enter 'node reverse_game.js'
